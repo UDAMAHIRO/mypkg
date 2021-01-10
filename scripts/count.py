18f9d77129b13a701df1700f73e50befec472d2a
Mahiro Uda <s19c1017as@s.chibakoudai.jp>
2021-01-10 11:18:56 +0000

    Add a file

---
+++ scripts/count.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32
+
+rospy.init_node('count')
+pub = rospy.Publisher('count_up', Int32, queue_size=1)
+rate = rospy.Rate(1)
+n = 1
+while not rospy.is_shutdown():
+    n += 1
+    if n == 10:
+        n = 1
+    pub.publish(n)
+    rate.sleep()
